d39f4bae16cf7deb4acda3bff06b8c9a69d5a032
Warren Harper II <warrenharper2@att.net>
2019-11-13 20:32:54 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,18 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+ PASSENGERS = {
+ SUITE_A: "AMANDA PRESLEY",
+ SUITE_B: "SEYMOUR HOFFMAN",
+ SUITE_C: "ALFRED TENNYSON",
+ SUITE_D: "CHARLIE CHAPLIN",
+ SUITE_E: "CRUMPET THE ELF"
+ }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+
+winner
 end
